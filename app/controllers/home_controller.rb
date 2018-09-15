d5427f2eb649a78cf7fba62860d9ef2249caf42d
WilliamMena <williammena1991@gmail.com>
2018-09-15 01:10:32 -0400

    Display conversion amount after pressing convert

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,60 @@
 require 'pry'
+require 'Home.rb'
 class HomeController < ApplicationController
 
+  # @rates = {
+  #   "USD_EUR": 0,
+  #   "USD_SGD": 0,
+  #   "EUR_USD": 0,
+  #   "EUR_SGD": 0,
+  #   "SGD_EUR": 0,
+  #   "SGD_USD": 0,
+  # }
+  #
+  @@rate = 0
+  @@convertedAmount = 0
+  @@conversion = ""
+
   def index
+    @amount = @@convertedAmount
   end
 
   def create
-    binding.pry
+    amount = params["amount"].to_i
+    from = params["currency_from"]
+    to = params["currency_to"]
+    @@conversion = from+'_'+to
+
+    # if Rails.cache.exist?(conversion)
+    #   @@rate = Rails.cache.read(conversion)
+    #   print "This was cached"
+    # else
+    #   @@rate = getCurrencyRate(from, to)
+    #   Rails.cache.write(conversion, rate, expires_in: 1.minute)
+    #   print "This is from the API"
+    # end
+    @@rate = getCurrencyRate(from, to)
+    @@convertedAmount = amount * @@rate
+    redirect_to :root
+
+
   end
 
   def show
   end
-end
 
-private
+  private
 
-  def home_params
-    binding.pry
+  def getCurrencyRate(from, to)
+    url = "https://free.currencyconverterapi.com/api/v6/convert?q=#{from}_#{to}&compact=y"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    return JSON.parse(response).first.last["val"]
   end
+
+
+
+
+
+
+end
--- app/models/home.rb
+++ app/models/home.rb
@@ -1,12 +1,14 @@
 require 'net/http'
 require 'json'
 
-class home
+class Home
 
   def getCurrencyRate(from, to)
     url = "https://free.currencyconverterapi.com/api/v6/convert?q=#{from}_#{to}&compact=y"
     uri = URI(url)
     response = Net::HTTP.get(uri)
     return JSON.parse(response).first.last["val"]
+
+  end
 
 end
