a3341e95821eee07c70ecd90b3368db531361719
WilliamMena <williammena1991@gmail.com>
2018-11-29 18:12:48 -0500

    Adjust session time to 15 minutes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_sheridan_stock_session', expire_after: 30.minutes
+Rails.application.config.session_store :cookie_store, key: '_sheridan_stock_session', expire_after: 15.minutes
